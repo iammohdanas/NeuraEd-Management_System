8dfb27ab93cb6bbc50951f2c8a7fcb2c87c7ec32
AATricks <aaanas372@gmail.com>
2024-01-27 17:39:49 +0530

    static_dirs changes

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
+# STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,9 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+# STATICFILES_DIRS=[
+# STATIC_DIR,
+#  ]
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 
